f2aa72a22de7c138824d4902bab9b32ca19253d0
Abdalla El Awad <abd.elawad@stud.uniroma3.it>
2025-05-29 12:57:07 +0200

    Initial commit

---
+++ src/main/java/it/uniroma3/siw/SiwBooks2Application.java
@@ -0,0 +1,13 @@
+package it.uniroma3.siw;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SiwBooks2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SiwBooks2Application.class, args);
+	}
+
+}
