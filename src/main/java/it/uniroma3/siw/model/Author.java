6de1904f8eb96cc62a6353cdf153e1f91ad274a4
Abdalla El Awad <abd.elawad@stud.uniroma3.it>
2025-06-11 15:37:36 +0200

    attributi d'interesse per il model Author

--- src/main/java/it/uniroma3/siw/model/Author.java
+++ src/main/java/it/uniroma3/siw/model/Author.java
@@ -1,8 +1,25 @@
 package it.uniroma3.siw.model;
 
+import java.time.LocalDate;
+
+import org.springframework.format.annotation.DateTimeFormat;
+
 import jakarta.persistence.Entity;
 
 @Entity
 public class Author {
+	private String name;
 
+	private String surname;
+
+	@DateTimeFormat(pattern = "yyyy-MM-dd")
+	private LocalDate dateOfBirth;
+	
+	//se non è (ancora) morto non serve settarlo, naturalmente
+	@DateTimeFormat(pattern = "yyyy-MM-dd")
+	private LocalDate dateOfDeath;
+	
+	private String nationality;
+	
+	//TODO gestione immagini (un autore ha UNA SOLA immagine)
 }
