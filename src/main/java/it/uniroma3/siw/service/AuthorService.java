e4552bf5807ba401b6d65baf2f6accdff9814600
Abdalla El Awad <abd.elawad@stud.uniroma3.it>
2025-06-17 18:48:59 +0200

    @Service annotato per AuthorService

--- src/main/java/it/uniroma3/siw/service/AuthorService.java
+++ src/main/java/it/uniroma3/siw/service/AuthorService.java
@@ -5,10 +5,11 @@ import java.util.List;
 import java.util.Set;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
 import it.uniroma3.siw.model.Author;
 import it.uniroma3.siw.repository.AuthorRepository;
-
+@Service
 public class AuthorService {
 	@Autowired
 	private AuthorRepository authorRepository;
