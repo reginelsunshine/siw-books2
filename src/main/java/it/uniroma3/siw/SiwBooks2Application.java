daf39abe7db5375fbafff4257917b829fc089fc5
Abdalla El Awad <abd.elawad@stud.uniroma3.it>
2025-06-26 11:57:25 +0200

    configurazione messaggi in app.prop

--- src/main/java/it/uniroma3/siw/SiwBooks2Application.java
+++ src/main/java/it/uniroma3/siw/SiwBooks2Application.java
@@ -1,13 +1,31 @@
 package it.uniroma3.siw;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
-public class SiwBooks2Application {
+import it.uniroma3.siw.model.Author;
+import it.uniroma3.siw.repository.AuthorRepository;
 
+@SpringBootApplication
+public class SiwBooks2Application implements CommandLineRunner{
+	@Autowired
+	AuthorRepository authorRepository;
 	public static void main(String[] args) {
 		SpringApplication.run(SiwBooks2Application.class, args);
 	}
-
+	
+	 public void run(String... args) throws Exception {
+		 List<Author> list = authorRepository.findAllByOrderBySurnameAscNameAsc();
+		 /*ora stampo con un for iterando la lista altrimenti stamperebbe solo gli oggetti autore e quindi gli indirizzi
+		   di memoria*/
+		 System.out.println("Lista ordinata per nome e cognome crescente:");
+		 for(Author autore : list) {
+			 //DA VERIFICARE!
+			 System.out.println(autore.getName()+""+autore.getSurname());
+		 }
+	 }
 }
--- src/main/java/it/uniroma3/siw/repository/AuthorRepository.java
+++ src/main/java/it/uniroma3/siw/repository/AuthorRepository.java
@@ -1,5 +1,6 @@
 package it.uniroma3.siw.repository;
 
+import java.util.List;
 import java.util.Optional;
 
 import org.springframework.data.repository.CrudRepository;
@@ -10,4 +11,7 @@ public interface AuthorRepository  extends CrudRepository<Author, Long>{
 	Optional<Author> findByNameAndSurname(String name, String surname);
 	
 	Optional<Author> findById(Long id);
+	
+	List<Author> findAllByOrderBySurnameAscNameAsc();
+
 }
